4daa0fe47ed7355f249585cb1dd9d8ad318039c2
tetrit <tema.aleew@yandex.ru>
2025-04-23 20:35:39 +0300

    starting work with lesson 2

---
+++ Lesson_2/app/src/main/java/ru/mirea/Aleev/Lesson2/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.mirea.Aleev.Lesson2;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
