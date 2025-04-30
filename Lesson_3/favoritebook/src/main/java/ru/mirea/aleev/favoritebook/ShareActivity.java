9930cf776b044286bedd02d692af93614faceba9
tetrit <tema.aleew@yandex.ru>
2025-04-30 20:51:48 +0300

    FavoriteBook Done

--- Lesson_3/favoritebook/src/main/java/ru/mirea/aleev/favoritebook/ShareActivity.java
+++ Lesson_3/favoritebook/src/main/java/ru/mirea/aleev/favoritebook/ShareActivity.java
@@ -1,6 +1,14 @@
 package ru.mirea.aleev.favoritebook;
 
+import static ru.mirea.aleev.favoritebook.MainActivity.BOOK_NAME_KEY;
+import static ru.mirea.aleev.favoritebook.MainActivity.QUOTES_KEY;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +18,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ShareActivity extends AppCompatActivity {
 
+    EditText editBook;
+    EditText editQuote;
+    Intent def_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +30,28 @@ public class ShareActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
             return insets;
+
         });
+
+
+        editBook = findViewById(R.id.editTextBook);
+        editQuote = findViewById(R.id.editTextQuote);
+
+        def_text = getIntent();
+
+        editBook.setText(def_text.getStringExtra(BOOK_NAME_KEY));
+        editQuote.setText(def_text.getStringExtra(QUOTES_KEY));
     }
+
+    public void onClickSubmit(View view){
+        Intent data = new Intent();
+        data.putExtra(MainActivity.USER_MESSAGE, "Название Вашей любимой книги: " + editBook.getText().toString() + ". Цитата: " + editBook.getText().toString());
+        setResult(Activity.RESULT_OK, data);
+        finish();
+    }
+
+
+
 }
