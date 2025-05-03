31ba153eb01a51c0da5b800438c3ea85ed6cae24
tetrit <tema.aleew@yandex.ru>
2025-05-03 21:24:03 +0300

    ServiceApp done

---
+++ rumireaAleevAVLesson4/serviceapp/src/main/java/ru/mirea/aleev/serviceapp/PlayerService.java
@@ -0,0 +1,75 @@
+package ru.mirea.aleev.serviceapp;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class PlayerService extends Service {
+    private MediaPlayer mediaPlayer;
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopForeground(true);
+            }
+        });
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+
+    @SuppressLint("ForegroundServiceType")
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentText("Playing daydream in blue")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("daydream in blue"))
+                .setContentTitle("Сейчас играет...");
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel(CHANNEL_ID, "Aleev AV Notification", importance);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel.setDescription("MIREA Channel");
+        }
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        assert channel != null;
+        notificationManager.createNotificationChannel(channel);
+        startForeground(1, builder.build());
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.daydreaminblue);
+        mediaPlayer.setLooping(false);
+
+
+
+    }
+    @Override
+    public void onDestroy() {
+        stopForeground(true);
+        mediaPlayer.stop();
+
+    }
+}
