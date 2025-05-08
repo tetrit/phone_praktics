94b9a40409bca8f6f6e6557ce8fd55ba0b6803c2
tetrit <tema.aleew@yandex.ru>
2025-05-08 23:15:52 +0300

    SecureSharedPreferences Done

--- rumireaAleevAVLesson6/securesharedpreferences/src/main/java/ru/mirea/aleev/securesharedpreferences/MainActivity.java
+++ rumireaAleevAVLesson6/securesharedpreferences/src/main/java/ru/mirea/aleev/securesharedpreferences/MainActivity.java
@@ -1,14 +1,29 @@
 package ru.mirea.aleev.securesharedpreferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKeys;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import ru.mirea.aleev.securesharedpreferences.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private TextView textView;
+    private SharedPreferences secureSharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        textView = binding.Maintext;
+        Button buttonSave = binding.button;
+
+        try {
+            String masterKey = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
+            secureSharedPreferences = EncryptedSharedPreferences.create(
+                    "secure_prefs",
+                    masterKey,
+                    this,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+        } catch (GeneralSecurityException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String WritterName = secureSharedPreferences.getString("WRITE_NAME", "Писатель: Филип К. Дик");
+        textView.setText(WritterName);
+
+        buttonSave.setOnClickListener(v ->{
+            secureSharedPreferences.edit().putString("WRITE_NAME", textView.getText().toString()).apply();
+            Toast.makeText(this, "Данные сохранены!", Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
