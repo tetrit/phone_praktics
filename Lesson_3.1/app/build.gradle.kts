5f91ccc119e99be1a356ab6f766a21ce13f9230d
tetrit <tema.aleew@yandex.ru>
2025-05-31 23:34:46 +0300

    DONE 7

--- Lesson_3.1/app/build.gradle.kts
+++ Lesson_3.1/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -43,6 +44,8 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.firebase.auth)
+    implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
